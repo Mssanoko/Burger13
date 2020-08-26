986699de8b66c92b632ef10bbf7eecfa6e64fa5b
Leslie Lawson <lawsonjleslie@gmail.com>
2020-08-26 08:00:37 -0400

    database mysql schema.sql & seeds.sql files js

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE burgerss_db;
+
+USE burgerss_db;
+
+CREATE TABLE burgers(
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(30) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY (id)
+)
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+USE burgerss_db;
+
+INSERT INTO burgers(burger_name, devoured)
+VALUES ("hamburger", false), ("cheeseburger",false), ("veggieburger", false);
