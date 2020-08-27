3b2bdbfec2bb96d2c3aa242d24c031e62a7e6d56
Leslie Lawson <lawsonjleslie@gmail.com>
2020-08-26 23:03:19 -0400

    server code for start server

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
 var express = require("express");
-var mysql = require("mysql");
-
 var app = express();
+var exphbs = require("express-handlebars");
+var bodyParser = require ("body-parser");
+var PORT = process.env.PORT || 3036;
 
-var PORT = process.env.PORT || 8080;
+app.listen(PORT, function(){
+  console.log("Server listening on: http://localhost:" + PORT);
+});
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "LeslieRoot",
-    database: "wizard_schools_db"
-  });
