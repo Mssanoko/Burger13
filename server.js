1fc960e1038cb21060adc21b30c5c80b75250999
Leslie Lawson <lawsonjleslie@gmail.com>
2020-08-26 23:18:26 -0400

    connection.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 var exphbs = require("express-handlebars");
 var bodyParser = require ("body-parser");
-var PORT = process.env.PORT || 3036;
+var PORT = process.env.PORT || 3306;
 
 
 // Parse application body as JSON
