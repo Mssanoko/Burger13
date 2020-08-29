663064417b37f9074e05e8a0228bf564c513d005
Leslie Lawson <lawsonjleslie@gmail.com>
2020-08-28 20:21:56 -0400

    deploy on heroku

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,9 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else {
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -8,7 +11,7 @@ var connection = mysql.createConnection({
   password: "LeslieRoot",
   database: "burgerss_db"
 });
-
+};
 // Make connection.
 connection.connect(function(err) {
   if (err) {
@@ -19,4 +22,5 @@ connection.connect(function(err) {
 });
 
 // Export connection for our ORM to use.
+connection.connect();
 module.exports = connection;
